5729a92cc4b7fcade8b229356d3023a09fc83851
RobinLucaBrusch <116346532+RobinLucaBrusch@users.noreply.github.com>
2023-05-30 13:24:45 +0200

    Add source to unit test

---
+++ Source/UnitTest1.cs
@@ -0,0 +1,55 @@
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Input string / Eingabe-String
+            string input = "abcabcbb";
+
+            // Length of the longest substring / Länge des längsten Substrings
+            int maxLength = 0;
+
+            // Length of the current substring / Länge des aktuellen Substrings
+            int currentLength = 0;
+
+            // Last occurrence of each character in the string / Letztes Vorkommen jedes Zeichens im String
+            int[] lastSeen = new int[256];
+
+            for (int i = 0; i < lastSeen.Length; i++)
+            {
+                lastSeen[i] = -1;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+
+                // If the character has not occurred before or the last occurrence is outside the current substring / Wenn das Zeichen noch nicht vorgekommen ist oder das letzte Vorkommen außerhalb des aktuellen Substrings liegt
+                if (lastSeen[currentChar] == -1 || i - currentLength > lastSeen[currentChar])
+                {
+                    currentLength++;
+                }
+                else
+                { //gleichsetzen
+                    if (currentLength > maxLength)
+                    {
+                        maxLength = currentLength;
+                    }
+                    currentLength = i - lastSeen[currentChar];
+                }
+                lastSeen[currentChar] = i;
+            }
+
+            if (currentLength > maxLength)
+            {
+                maxLength = currentLength;
+            }
+
+            // Assert that the length of the longest substring is correct / Überprüfen, ob die Länge des längsten Substrings korrekt ist
+            Assert.AreEqual(3, maxLength);
+        }
+    }
+}
